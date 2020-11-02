7c5f1ae3f25c168439b91610f4ed97c00929f6ec
Jack Le <jackle61333@gmail.com>
2020-11-02 14:25:35 -0600

    bruh

--- Tag-Cloud-Project/src/Scraper.java
+++ Tag-Cloud-Project/src/Scraper.java
@@ -11,14 +11,23 @@ public class Scraper  {
 			site.connect();
 			Scanner reader = new Scanner( site.getInputStream() );
 			System.out.println( reader.useDelimiter( "\\Z" ).next() );
+			reader.close();
 		}
 		catch (MalformedURLException e) {
-		// new URL() failed
+			// new URL() failed
 			// ...
-			}
-			catch (IOException e) {
+			System.out.println("malformed");
+			StringWriter sw = new StringWriter();
+			e.printStackTrace(new PrintWriter(sw));
+			System.out.println(sw.toString());
+		}
+		catch (IOException e) {
 			// openConnection() failed
 			// ...
-			}
+			System.out.println("not open");
+			StringWriter sw = new StringWriter();
+			e.printStackTrace(new PrintWriter(sw));
+			System.out.println(sw.toString());
+		}
 	}
 }
