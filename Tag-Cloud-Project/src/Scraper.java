194d856ac3f83e41c22048120a7715db874ffe37
Jack Le <jackle61333@gmail.com>
2020-11-04 14:35:00 -0600

    fsafasf

--- Tag-Cloud-Project/src/Scraper.java
+++ Tag-Cloud-Project/src/Scraper.java
@@ -1,22 +1,32 @@
 import java.util.*;
+import java.util.regex.*;
 import java.io.*;
 import java.net.*;
 
 public class Scraper  {
 	
-	public static void main(String[] args) {
+	private static Pattern p = Pattern.compile("(<(.+?)>(.+?)<(.+?)>)");
+	private static String URL;
+	private static String website;
+	
+	public Scraper(String url) {
+		this.URL = url;
+		setup();
+	}
+	
+	private void setup() {
 		try {
-			URL myURL = new URL("http://endless.horse/");
+			URL myURL = new URL("http://books.toscrape.com/");
 			URLConnection site = myURL.openConnection();
 			site.connect();
 			Scanner reader = new Scanner( site.getInputStream() );
-			System.out.println( reader.useDelimiter( "\\Z" ).next() );
+			website = ( reader.useDelimiter( "\\Z" ).next() );
 			reader.close();
 		}
 		catch (MalformedURLException e) {
 			// new URL() failed
 			// ...
-			System.out.println("malformed");
+			System.out.println("Malformed URL");
 			StringWriter sw = new StringWriter();
 			e.printStackTrace(new PrintWriter(sw));
 			System.out.println(sw.toString());
@@ -24,10 +34,13 @@ public class Scraper  {
 		catch (IOException e) {
 			// openConnection() failed
 			// ...
-			System.out.println("not open");
+			System.out.println("Connection couldn't be opened");
 			StringWriter sw = new StringWriter();
 			e.printStackTrace(new PrintWriter(sw));
 			System.out.println(sw.toString());
 		}
+		
+		
+		
 	}
 }
--- Tag-Cloud-Project/src/Test.java
+++ Tag-Cloud-Project/src/Test.java
@@ -1,11 +1,36 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Test {
+	
+	private static Pattern p = Pattern.compile("(<(.+?)>(.+?)<(.+?)>)");
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		System.out.println("pp");
-		System.out.println("hello 123");
+		String s = "fsafsafafs<p>this is between the tags<\\p>fsafsafafs<p>fafsfthis is between the tags<\\\\p>fsafsafafs<p>this is behdshdshtween the tags<\\\\p>fsafsafafs<p>this is bggdsetween the tags<\\\\p>";
+		
+		System.out.println(getTagValues(s));
+		
+	}
+	
+	private static List<String> getTagValues(final String str) {
+	    final List<String> tagValues = new ArrayList<String>();
+	    final Matcher matcher = p.matcher(str);
+	    while (matcher.find()) {
+	        tagValues.add(matcher.group(1));
+	    }
+	    return tagValues;
+	}
+	
+	private class Word {
+		int weight;
+		String word;
+		
+		public Word(int weight, String word) {
+			this.weight = weight;
+			this.word = word;
+		}
 	}
 
 }
