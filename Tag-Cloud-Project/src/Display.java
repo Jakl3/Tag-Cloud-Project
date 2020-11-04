751af715af23dfbfbdde36f692b859803d6ae5d8
Nathan Nguyen <Nathan Nguyen@NN.attlocal.net>
2020-11-04 17:14:40 -0600

    added data & scraper to graphics

--- Tag-Cloud-Project/src/Display.java
+++ Tag-Cloud-Project/src/Display.java
@@ -2,16 +2,26 @@ import java.awt.*;
 import java.util.*;
 
 public class Display extends Canvas{
-	Map<String,Integer> words;
+	Map<Integer, HashSet<String>> words;
 	public Display(Map<String,Integer> w) {
-		words = w;
+		//rank words by rank
+		words = new TreeMap<Integer,HashSet<String>>(Collections.reverseOrder());
+		for(Map.Entry<String,Integer> k : w.entrySet()) {
+			if(!words.containsKey(k.getValue())) {
+				words.put(k.getValue(), new HashSet<String>());
+			}
+			words.get(k.getValue()).add(k.getKey());
+		}
+		System.out.println(words);
 		setBackground(Color.black);
 	}
 	public void paint(Graphics w) {
-		w.setColor(Color.green);
-		for(String e : words.keySet()) {
-			w.setFont(new Font("SANS_SERIF",Font.ITALIC,words.get(e)*5));
-			w.drawString(e, (int)(Math.random()*1100), (int)(Math.random()*900));
+		w.setColor(Color.CYAN);
+		for(Integer e : words.keySet()) {
+			w.setFont(new Font("Gulim",Font.PLAIN,e*10));
+			for(String a : words.get(e)) {
+				w.drawString(a, (int)(Math.random()*1100), (int)(Math.random()*900));
+			}
 		}
 	}
 }
