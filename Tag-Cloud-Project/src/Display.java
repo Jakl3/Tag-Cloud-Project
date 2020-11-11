5aafa732f3dd96e69ac380b9a82b6108e5fee01a
Nathan Nguyen <Nathan Nguyen@NN.attlocal.net>
2020-11-11 17:41:34 -0600

    starfish boi

--- Tag-Cloud-Project/src/Display.java
+++ Tag-Cloud-Project/src/Display.java
@@ -28,49 +28,52 @@ public class Display extends Canvas{
 		ArrayList<Word> drawnWords = new ArrayList<Word>();
 		//use to check intersects stuff later
 		w.setColor(Color.CYAN);
+		int i = 0;
 		for(Word e : words) {
 			//w.setFont(new Font("Gulim",Font.PLAIN,e));
 			w.setFont(e.getFont());
 			FontMetrics pp = w.getFontMetrics();
 			int width = pp.stringWidth(e.getWord());
-			int height = pp.getMaxAscent();
-			int i = 0;
-			int[] dx = {1,1,1,0,0,-1,-1,-1};
-			int[] dy = {-1,0,1,1,-1,-1,0,1};
-			int x = (int)(Math.random()*800), y = (int)(Math.random()*800);
-			Rectangle r = new Rectangle(x,y,width,height);
+			//int height = pp.getMaxAscent();
+			int height = (int) pp.getLineMetrics(e.getWord(), w).getHeight();
+//			int[] dx = {1,1,1,0,0,-1,-1,-1};
+//			int[] dy = {-1,0,1,1,-1,-1,0,1};
+			int[] dx = {0,-1,-1,-1,0,1,1,1};
+			int[] dy = {-1,-1,0,1,1,1,0,-1};
+//			int x = (int)(Math.random()*800), y = (int)(Math.random()*800);
+			int x = 400, y = 400;
+			//Rectangle r = new Rectangle(x,y,width,height);
 			e.setDimensions(x, y, width, height);
-			drawnWords.add(e);
-			w.drawRect(x, y, width, height);
-			w.drawString(e.getWord(), x, y+height);
 				//int width = w.getFontMetrics().stringWidth(a);	
 				//int xcor = (int)(Math.random()*800), ycor = (int)(Math.random()*800);
-				
 				//Rectangle r = new Rectangle(X, Y, width, height);
-//				boolean ok = false;
-//				while(!ok) {
-//					boolean dab = false;
-//					for(Rectangle item : rectangles) {
-//						if(r.intersects(item)) {
-//							break;
-//						}
-//					}
-//					X += dx[i];
-//					Y += dy[i];
-//				}
-				
+				boolean ok = false;
+				if(drawnWords.size()>0) {
+				while(!ok) {
+					for(Word item : drawnWords) {
+						if(e.intersects(item)) {
+							ok = false;
+							break;
+						}
+						else {
+							ok = true;
+						}
+					}
+					if(ok) { break; } 
+					e.changeX(dx[i]);
+					e.changeY(dy[i]);
+					}
+				}
 //				w.drawRect(r.x,r.y,r.width,r.height);
-//				rectangles.add(r);
-				
-				
-				//w.drawString(a, xcor,ycor);
-				
-				//System.out.println(pp.getStringBounds(a, w));
-//				
-//				
-//				i++;
-//				i%=8;
+//				rectangles.add(r);	
+//				w.drawString(a, xcor,ycor);
+//				System.out.println(pp.getStringBounds(a, w));
 			
+				drawnWords.add(e);
+				w.drawRect(e.x, e.y, e.width, e.height);
+				w.drawString(e.getWord(), e.x, e.y+height);
+				i++;
+				i%=8;
 		}
 	}
 }
